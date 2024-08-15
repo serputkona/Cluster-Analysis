5f3e208ee8e845e2b9b3a3e1c349d9607c8e9ee1
Anastasiia <138507200+serputkona@users.noreply.github.com>
2024-08-15 16:15:34 -0400

    Update Cluster_analysis.R

--- Cluster_analysis.R
+++ Cluster_analysis.R
@@ -1,66 +1,143 @@
 getwd()
-Suicidal_Behaviours  <- read.csv("GHSH_Pooled_Data1.csv")
+my_data  <- read.csv("perfume_data.csv")
+my_data
 
-#1
-seredn <- mean(Suicidal_Behaviours$Have_Understanding_Parents)
-seredn
-median(Suicidal_Behaviours$Have_Understanding_Parents)
+data <- scale(my_data)
+head(data)
+
+library("cluster")
+library("ggplot2")
+library("factoextra")
+
+"""
+Функція fviz_dist() з пакету factoextra візуалізує матрицю відстаней
+Аргумент gradient , список з трьох елементів, визначає кольори для зображення малих, середніх та великих значень відстаней.
+
+Статистика Гопкінса показує, що він є більш-менш явно розділеним на кластери
+"""
+data.dist <- get_dist(data, method = "pearson")
+fviz_dist(data.dist, gradient = list(low = "salmon", mid = "white", high = "yellow"))
+
+library(hopkins)
+hopkins(data)
+
+"""
+Ієрархічна кластеризація
+Найкраща кількість кластерів – це кількість вертикальних ліній в дендрограмі,
+що перетинаються горизонтальними лініями, відстань між якими є перевернутою
+максимальною вертикальною відстанню без перетину кластеру.
+"""
+data.dist <- get_dist(data, stand = TRUE)
+data.hc1 <- hclust(data.dist) # кластеризація
+fviz_dend(data.hc1)
 
 
-#2
-var(Suicidal_Behaviours$Have_Understanding_Parents) #дисперсія
-stand_vidh <- sd(Suicidal_Behaviours$Have_Understanding_Parents) #стандартне відхилення
-stand_vidh
-cv <- stand_vidh / seredn * 100 #коефіцієнт варіації
-cv
-min_ <- min(Suicidal_Behaviours$Have_Understanding_Parents)
-max_ <- max(Suicidal_Behaviours$Have_Understanding_Parents)
-max_ - min_
-IQR(Suicidal_Behaviours$Have_Understanding_Parents) #інтерквальний розмах
+fviz_dend(data.hc1, k = 2, # розділяємо на 2 кластери
+ cex = 0.5, # розмір шрифту для назв об'єктів
+ k_colors = c("purple3", "orange1"), # кольори для різних кластерів
+ color_labels_by_k = TRUE, # назви груп також будуть різного кольору
+ rect = TRUE, # прямокутник, що виділяє кожну групу
+ main = "Дендрограма для кластеризації за методом дальнього сусіда")
+
+fviz_dend(data.hc1, k = 4, # розділяємо на 4 кластери
+ cex = 0.5, # розмір шрифту для назв об'єктів
+ k_colors = c("purple3", "orange1"), # кольори для різних кластерів
+ color_labels_by_k = TRUE, # назви груп також будуть різного кольору
+ rect = TRUE, # прямокутник, що виділяє кожну групу
+ main = "Дендрограма для кластеризації за методом дальнього сусіда")
+
+data_cl1 <- hclust(data.dist, method = "ward.D2")
+fviz_dend(data_cl1, cex = 0.2)
+          
+          
+fviz_dend(data_cl1, k = 2,
+ cex = 1,
+ k_colors = c("cyan2", "palevioletred", "yellow1"),
+ color_labels_by_k = TRUE,
+ rect = TRUE,
+ main = "Дендрограма для кластеризації за методом Варда (2 кластери)")
 
 
-#3
-boxplot(Suicidal_Behaviours$Have_Understanding_Parents,
-        main="Box-and-whiskers plot:Adolescents with Suicidal Behaviours
-        and Understanding with their parents   ",ylab="Percents(%)", col="coral2")
+fviz_dend(data_cl1, k = 4,
+ cex = 1,
+ k_colors = c("cyan2", "palevioletred", "yellow1"),
+ color_labels_by_k = TRUE,
+ rect = TRUE,
+ main = "Дендрограма для кластеризації за методом Варда (4 кластери)")
 
-#4
-quantile(Suicidal_Behaviours$Have_Understanding_Parents)
 
-#5
-quantile(Suicidal_Behaviours$Have_Understanding_Parents, prob = c(0.1, 0.9)) 
+#Ітераційний метод кластеризації - k-медоїдів
+data_cl2 <- pam(data.dist, 2)
+data_cl2$data = data.dist
+fviz_cluster(data_cl2, palette = "jco")
 
-#6
-library(psych)
-describe(Suicidal_Behaviours$Have_Understanding_Parents)[11] #коефіцієнт асиметрії
-describe(Suicidal_Behaviours$Have_Understanding_Parents)[12] #коефіцієнт ексцесу
 
-#7
-hist(Suicidal_Behaviours$Have_Understanding_Parents,
-     freq=F, main="Sturges rule", col="darkorchid1")
-hist(Suicidal_Behaviours$Have_Understanding_Parents, breaks = "Scott",
-     freq=F, main="Scott rule", col = "lightpink1", 
-     xlab = "Understanding with parents", ylab = "relative frequency")
-hist(Suicidal_Behaviours$Have_Understanding_Parents, breaks = "FD", freq=F, main="FD rule",
-     col = "darkseagreen3", xlab = "Understanding with parents", ylab = "relative frequency")
-lines(density(Suicidal_Behaviours$Have_Understanding_Parents), col='chocolate2', lwd=5)
-plot(density(Suicidal_Behaviours$Have_Understanding_Parents), col='cornflowerblue', lwd=5) #графік щільності
+fviz_cluster(data_cl2, ellipse.type = "t", palette = "jco")
 
-#8
-library(car)
-qqnorm(Suicidal_Behaviours$Have_Understanding_Parents, pch = 3, frame = FALSE, col="lightblue4")
-qqline(Suicidal_Behaviours$Have_Understanding_Parents, col = "lightcoral", lwd = 3)
+#PAM є робастим до шумів і викидів
+data_pam <- eclust(data, "pam", k = 2, stand = FALSE)
 
-#9
-plot(pnorm(sort(x)), (1:length(x))/length(x),
-     xlab="Теоретична функція розподілу", ylab = "Емпірична функція
-розподілу", asp = 1)
-abline(0, 1, col="red")
 
-plot(pnorm(sort((Suicidal_Behaviours$Have_Understanding_Parents  - mean(Suicidal_Behaviours$Have_Understanding_Parents)) / sd(Suicidal_Behaviours$Have_Understanding_Parents))),
-     (1:length(Suicidal_Behaviours$Have_Understanding_Parents)) / length(Suicidal_Behaviours$Have_Understanding_Parents),xlab="Теоретична функція розподілу",ylab="Імпірична функція розподілу",asp=1)
-abline(0, 1, col="green2") 
+#Зобразимо діаграму, яка зображує значення силуету для кожної точки та середнє значення силуету.
+arrests.kmeans2 <- kmeans(data, centers = 2, nstart = 20)
+sil2 <- silhouette(arrests.kmeans2$cluster, dist(data))
+fviz_silhouette(sil2)
 
-#10
-shapiro.test(Suicidal_Behaviours$Have_Understanding_Parents)  
-#критерій згоди Колмогорова-Смірнова для неперервних розподілів та критерій нормальності Шапіро-Уілка.
+"""
+Додатнє значення силуету означає, що об’єкт розміщений у потрібному кластері, і чим ближче
+значення до 1, тим краще він розміщений. Наявні кілька від’ємних значень силуетів, що означає що об’єкт знаходиться не в тому кластері і є нульві силуети, тобто розміщені на межі між двома кластерами.
+"""
+
+#Визначення оптимальної кількості кластерів
+
+#1) Метод ліктя
+#Метод ліктя спирається на порівняння внутрішньокластерної суми квадратів відстаней (WSS), для зручності ця величина зображується як функція від кількості кластерів.
+
+library(NbClust)
+fviz_nbclust(data, kmeans, method = "wss") +
+ geom_vline(xintercept = 2, linetype = 2) +
+ labs(subtitle = "Метод ліктя")
+
+#Оскільки останнє суттєве зменшення внутрішньокластерної суми квадратів досягається в точці 2, можна припустити що оптимальна кількість кластерів - 2. 
+
+#2) Метод середнього силуету
+#Метод середнього силуету дещо інакше характеризує якість кластеризації, і визначає, наскільки “вдало” кожна точка розміщена в своєму кластері. Оптимальною кількістю кластерів є така, що максимізує середнє значення силуету.
+
+fviz_nbclust(my_data, kmeans, method = "silhouette") +
+labs(subtitle = "Метод середнього силуету")
+
+#У методі середньогго силету оптимальною кількістю кластерів є така, що максимізує середнє значення силуету, а отже знову точка 2.
+
+#3. Метод статистики розриву
+#Оптимальною кількістю кластерів, згідно логіці цього методу, є така, яка максимізує значення статистики розриву. Це означатиме, що структура кластерів суттєво відрізняється від випадкового рівномірного розподілу точок (об’єктів).
+
+fviz_nbclust(my_data, kmeans, nstart = 25, method = "gap_stat",
+ nboot = 500 
+)+
+ labs(subtitle = "Метод статистики розриву")
+
+#Всі три емтоди дали однаковий результат - 2, отже робимо висновок що оптимальна кількість кластерів все ж 2.
+
+library(fpc)
+#к метоїдів  і Варда
+dd <- dist(data, method ="euclidean")
+data_pam <- eclust(data, "pam", k = 2, stand = FALSE)
+clust.arrests.ward2 <- cutree(hclust(dd, method = "ward.D2"), 2)
+km_stats3 <-  cluster.stats(dd,data_pam$cluster,
+                alt.clustering = clust.arrests.ward2)
+km_stats3$corrected.rand
+
+#Варда і середнього
+ward_data <- cutree(hclust(dd, method = "ward.D2"), 2)
+data.kmeans3<-kmeans(my_data, centers = 2, nstart = 10)
+km_stats3 <- cluster.stats(dd,data.kmeans3$cluster)
+km_stats2 <- cluster.stats(dd, data.kmeans3$cluster,alt.clustering = ward_data)
+km_stats2$corrected.rand
+
+#середн і метоїди
+
+data.kmeans3<-kmeans(my_data, centers = 2, nstart = 10)
+data_pam <- eclust(data, "pam", k = 2, stand = FALSE)
+
+km_stats5<-cluster.stats(dd,data_pam$cluster,data.kmeans3$cluster)
+km_stats5$corrected.rand
